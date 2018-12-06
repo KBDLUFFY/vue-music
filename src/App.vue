e117978e264b61b2d01fc59735a895862cc9061c
CodingBecky <tanxx@noahark.com.cn>
2018-12-06 17:20:42 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    Hello World!
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import "common/stylus/variable"
+#app
+  color: $color-theme
+</style>
+
---
+++ src/main.js
@@ -0,0 +1,25 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+//插件安装
+// axios
+import axios from 'axios' 
+Vue.prototype.$axios = axios
+// flexible
+import 'lib-flexible/flexible'
+// 移动端点击延迟300毫秒解决方案
+import FastClick from 'fastclick'
+FastClick.attach(document.body);
+//引入样式文件
+import 'common/stylus/index.styl'
+
+Vue.config.productionTip = false
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
